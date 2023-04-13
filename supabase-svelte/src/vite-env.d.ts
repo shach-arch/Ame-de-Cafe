5fd2e81ced3b293626ed0c31fd40e94d80b83c93
shach-arch <66858792+shach-arch@users.noreply.github.com>
2023-04-13 14:58:09 +0800

    small change

--- supabase-svelte/src/vite-env.d.ts
+++ supabase-svelte/src/vite-env.d.ts
@@ -1,12 +1,5 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-interface ImportMetaEnv {
-    VITE_SUPABASE_URL: string;
-    VITE_SUPABASE_ANON_KEY: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+
   
   
