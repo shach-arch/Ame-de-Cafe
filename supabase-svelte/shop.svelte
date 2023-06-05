ceba781a5f7deb976d8bfa76633edd9a8c9bb1d9
shach-arch <66858792+shach-arch@users.noreply.github.com>
2023-06-05 13:25:25 +0800

    need to get price working

--- supabase-svelte/shop.svelte
+++ supabase-svelte/shop.svelte
@@ -1,7 +1,8 @@
 <script>
   import { createClient } from "@supabase/supabase-js";
   import { onMount } from "svelte";
-  import { readable, get } from "svelte/store";
+  import { writable, get } from "svelte/store";
+
 
   const url = "https://ujnattukwsqsjmzuhyoh.supabase.co";
 
@@ -9,6 +10,7 @@
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqbmF0dHVrd3Nxc2ptenVoeW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU1OTUyMDgsImV4cCI6MjAwMTE3MTIwOH0.G9dIZVpPY5ylDQD63YRjGFGuP3LGtECuQndt8OdxtZM";
 
   const supabase = createClient(url, anonKey);
+  
   
   async function fetchAll() {
     let { data: Products, error } = await supabase.from("Products").select("*");
@@ -22,63 +24,38 @@
 
   fetchAll();
 
-  let selectedCoffee = "";
-  let selectedDonut = "";
-  let totalPrice = 0;
 
-  const productOptions = readable([], (set) => {
-    fetch(
-      "https://api.supabase.io/v1/table/product?select=*&product_group=eq.{productGroup}",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization:
-            "Bearer ZjMfEockQY0LjZm3KmajSkZHVRPNRQHPABeM73lbWCvQTk1GriNVjg7Q9qqOB0d/2Y5HNz++1ds3x7AuGn78pg==",
-        },
-        mode: "no-cors",
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        return response.json();
-      })
-      .then((data) => set(data))
-      .catch((error) => console.error(error));
-  });
+  const coffeeOptions = writable([]);// Declare and initialize coffeeOptions variable
+  const donutOptions = writable([]);// Declare and initialize donutOptions variable
+  const totalPrice = writable(0);// Declare and initialize totalPrice variable
 
-  const coffeeOptions = readable([], (set) => {
-    const unsubscribe = productOptions.subscribe((value) => {
-      const options = value.filter(
-        (product) => product.product_group === "coffee"
-      );
-      set(options);
-    });
-    return unsubscribe;
-  });
+  let selectedDonut = ""; // Declare and initialize selectedDonut variable
+  let selectedCoffee = ""; // Declare and initialize selectedCoffee variable
 
-  const donutOptions = readable([], (set) => {
-    const unsubscribe = productOptions.subscribe((value) => {
-      const options = value.filter(
-        (product) => product.product_group === "food"
-      );
-      set(options);
-    });
-    return unsubscribe;
-  });
 
-  function updateTotalPrice() {
-    const coffeePrice =
-      get(coffeeOptions).find((coffee) => coffee.name === selectedCoffee)
-        ?.price || 0;
-    const donutPrice =
-      get(donutOptions).find((donut) => donut.name === selectedDonut)?.price ||
-      0;
-    totalPrice = coffeePrice + donutPrice;
+  // Update the total price when the selected coffee or donut changes
+  async function updateTotalPrice() {
+  try {
+    const { data: coffeeData } = await supabase
+      .from('Products')
+      .select('product,current_retail_price')
+      .match({ product_group: 'Whole Bean/Teas' });
+
+    const selectedCoffeePrice = coffeeData.find((coffee) => coffee.product === selectedCoffee)?.current_retail_price || 0;
+
+    const { data: donutData } = await supabase
+      .from('Products')
+      .select('product,current_retail_price')
+      .match({ product_group: 'Food' });
+
+    const donutPrice = donutData.find((donut) => donut.product === selectedDonut)?.current_retail_price || 0;
+
+    totalPrice.set(parseFloat(selectedCoffeePrice) + parseFloat(donutPrice));
+  } catch (error) {
+    console.error('Error fetching data from Supabase:', error);
   }
-
+}
+  
   function submitOrder() {
     // Handle submitting the order
     console.log("Order submitted!");
@@ -87,6 +64,28 @@
     console.log("Total price:", totalPrice);
   }
 
+ // Fetch products that match the specified product groups
+const coffeeGroups = ['Whole Bean/Teas', 'Beverages'];
+
+supabase
+  .from('Products')
+  .select('product')
+  .in('product_group', coffeeGroups)
+  .then(({ data }) => {
+    const coffeeProducts = data.map((item) => item.product);
+    coffeeOptions.set(coffeeProducts);
+  })
+
+// Fetch products that match the specified product groups
+const donutGroups = ['Food'];  
+supabase
+  .from('Products')
+  .select('product')
+  .in('product_group', donutGroups)
+  .then(({ data }) => {
+    const donutProducts = data.map((item) => item.product);
+    donutOptions.set(donutProducts);
+  })  
   onMount(() => {
     updateTotalPrice();
   });
@@ -97,30 +96,25 @@
 
   <form on:submit|preventDefault={submitOrder}>
     <label for="coffee">Select Coffee:</label>
-    <select
-      id="coffee"
-      bind:value={selectedCoffee}
-      on:change={updateTotalPrice}
-    >
+    <select id="coffee" bind:value={selectedCoffee} on:change={updateTotalPrice}>
       <option value="">-- Choose a coffee --</option>
       {#each $coffeeOptions as coffee}
-        <option value={coffee.name}>{coffee.name}</option>
+        <option value={coffee}>{coffee}</option>
       {/each}
     </select>
-
+  
     <label for="donut">Select Donut:</label>
     <select id="donut" bind:value={selectedDonut} on:change={updateTotalPrice}>
       <option value="">-- Choose a donut --</option>
       {#each $donutOptions as donut}
-        <option value={donut.name}>{donut.name}</option>
+        <option value={donut}>{donut}</option>
       {/each}
     </select>
+  
+    <p>Total Price: ${$totalPrice.toFixed(2)}</p>
 
-    <p>Total Price: ${totalPrice.toFixed(2)}</p>
-
-    <button type="submit" disabled={!(selectedCoffee && selectedDonut)}
-      >Place Order</button
-    >
+  
+    <button type="submit" disabled={!(selectedCoffee && selectedDonut)}>Place Order</button>
   </form>
 </main>
 
