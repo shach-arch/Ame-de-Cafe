f8f2df622900f6d9ff1289db1f9e57f670a5d076
shach-arch <66858792+shach-arch@users.noreply.github.com>
2023-06-12 10:35:13 +0800

    style for login and signup added

--- supabase-svelte/login.svelte
+++ supabase-svelte/login.svelte
@@ -1,6 +1,8 @@
 <!-- Login.html -->
 
 <script>
+// @ts-nocheck
+
   import { AuthError, createClient } from '@supabase/supabase-js';
   import{navigate} from 'svelte-routing';
   import 'bootstrap/dist/js/bootstrap.min.js';
@@ -39,64 +41,89 @@
 </script>
 
 <main>
-  <style>
-      @import 'bootstrap/dist/css/bootstrap.min.css';
-    main {
-      max-width: 800px;
-      margin: 0 auto;
-      padding: 2rem;
-      align : center;
-    }
-    h1 {
-      text-align: center;
-    }
-    body {
-     margin: 0 auto;
-    }
-    form {
-      display: inline;
-    }
-    label {
-      display: block;
-      margin-bottom: 0.5rem;
-    }
-    input {
-      padding: 0.5rem;
-      margin-bottom: 1rem;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 1rem;
-    }
-    button {
-      padding: 0.5rem;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 1rem;
-      background-color: #fff;
-      cursor: pointer;
-    }
-    button:hover {
-      background-color: #f5f5f5;
-    }
+ <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Ame de cafe Express</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" type="text/css" href="src/css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" type="text/css" href="src/css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="src/css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="src/images/fevicon.png" type="image/gif" />
+      <!-- font css -->
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
+ </head>
 
-  </style>
-  <h1>Login</h1>
-<body>
-  <form on:submit|preventDefault={login}>
-    <p><label for="email">Email:</label>
-      <input type="email" id="email" bind:value={email} required />
-    </p>
-    <p>
-      <label for="password">Password:</label>
-    <input type="password" id="password" bind:value={password} required />
-    </p>
-    <p><button type="submit">Log in</button>
-    </p>
-  </form>
-</body>
+ <body>
+  <div class="header_section">
+    <div class="container-fluid">
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a class="navbar-brand"><img src="src/images/favicon.ico"></a>
+          <p class="logo_text">Ame de cafe Express</p>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="form-inline my-2 my-lg-0">
+            </form>
+         </div>
+      </nav>
+   </div>
+           <!-- banner section start --> 
+           <div class="banner_section layout_padding">
+            <div class="container-fluid">
+              <div id="banner_slider" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <!-- svelte-ignore a11y-missing-attribute -->
+                        <div class="banner_img"><img src="src/images/banner-img.png"></div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="banner_taital_main">
+                          <h1 class="banner_taital">Login</h1>
+                          <h5 class="tasty_text">Tasty Of Ame de cafe Express</h5>
+                          <div class="btn_main">
+                            <div class="about_bt">
+                              <form on:submit|preventDefault={login}>
+                                <div class="mb-3">
+                                  <label for="email" class="form-label">Email address</label>
+                                  <input type="email" class="form-control" id="email" bind:value={email} required>
+                                </div>
+                                <div class="mb-3">
+                                  <label for="password" class="form-label">Password</label>
+                                  <input type="password" class="form-control" id="password" bind:value={password} required>
+                                </div>
+                                <div class="d-flex align-items-center justify-content-between">
+                                  <button type="submit" class="btn btn-primary">Login</button>
+                                  <button type="button" class="btn btn-primary" onclick="window.location.href = 'signup.html'">signup</button>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+ </body>
 
-{#if loginMessage}
-  <p>{loginMessage}</p>
-{/if}
-  <p>Need an account? <a href="signup.html">Sign up here</a>.</p>
 </main>
--- supabase-svelte/signup.svelte
+++ supabase-svelte/signup.svelte
@@ -1,4 +1,6 @@
 <script>
+// @ts-nocheck
+
   import { createClient } from '@supabase/supabase-js';
   import { navigate } from 'svelte-routing';
 
@@ -47,60 +49,96 @@
 
 <main>
 
-  <style>
-    main {
-      max-width: 800px;
-      margin: 0 auto;
-      padding: 2rem;
-      align : center;
-    }
-    h1 {
-      text-align: center;
-    }
-    body {
-     margin: 0 auto;
-    }
-    form {
-      display: inline;
-    }
-    label {
-      display: block;
-      margin-bottom: 0.5rem;
-    }
-    input {
-      padding: 0.5rem;
-      margin-bottom: 1rem;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 1rem;
-    }
-    button {
-      padding: 0.5rem;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 1rem;
-      background-color: #fff;
-      cursor: pointer;
-    }
-    button:hover {
-      background-color: #f5f5f5;
-    }
-
-  </style>
-  <h1>Sign Up</h1>
-  {#if successMessage}
-    <p class="success">{successMessage}</p>
-  {:else if errorMessage}
-    <p class="error">{errorMessage}</p>
-  {/if}
-  <form on:submit={handleSubmit}>
-    <label for="email">Email:</label>
-    <input type="email" id="email" bind:value={email}>
-    <label for="password">Password:</label>
-    <input type="password" id="password" bind:value={password}>
-    <label for="confirmPassword">Confirm Password:</label>
-    <input type="password" id="confirmPassword" bind:value={confirmPassword}>
-    <button type="submit">Sign Up</button>
-  </form>
-  <p>Already have an account? <a href="index.html">Log in here</a>.</p>
+  <head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>Ame de cafe Express</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" type="text/css" href="src/css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" type="text/css" href="src/css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="src/css/responsive.css">
+    <!-- fevicon -->
+    <link rel="icon" href="src/images/fevicon.png" type="image/gif" />
+    <!-- font css -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="header_section">
+    <div class="container-fluid">
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a class="navbar-brand"><img src="src/images/favicon.ico"></a>
+          <p class="logo_text">Ame de cafe Express</p>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="form-inline my-2 my-lg-0">
+            </form>
+         </div>
+      </nav>
+   </div>
+ <!-- banner section start --> 
+ <div class="banner_section layout_padding">
+  <div class="container-fluid">
+    <div id="banner_slider" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-md-6">
+              <!-- svelte-ignore a11y-missing-attribute -->
+              <div class="banner_img"><img src="src/images/banner-img.png"></div>
+            </div>
+            <div class="col-md-6">
+              <div class="banner_taital_main">
+                <h1 class="banner_taital">Signup</h1>
+                <h5 class="tasty_text">Tasty Of Ame de cafe Express</h5>
+                <div class="btn_main">
+                  <div class="about_bt">
+                    {#if successMessage}
+                      <p class="success">{successMessage}</p>
+                    {:else if errorMessage}
+                      <p class="error">{errorMessage}</p>
+                    {/if}
+                    <form on:submit={handleSubmit}>
+                      <div class="mb-3">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" bind:value={email}>
+                      </div>
+                      <div class="mb-3">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" bind:value={password}>
+                      </div>
+                      <div class="mb-3">
+                        <label for="confirmPassword">Confirm Password:</label>
+                        <input type="password" id="confirmPassword" bind:value={confirmPassword}>
+                      </div>
+                      <div class="d-flex align-items-center justify-content-between">
+                        <button type="submit" class="btn btn-primary">Sign Up</button>
+                        <button type="button" class="btn btn-primary" onclick="window.location.href = 'index.html'">login</button>
+                      </div>
+                    </form>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
 </main>
