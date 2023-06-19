f57d07d354e15c1f6d81a4f301f97226c8ad933b
Ché Alexander <cheshaquan@gmaiil.com>
2023-06-19 15:20:58 +0800

    fix bug

--- supabase-svelte/shop.svelte
+++ supabase-svelte/shop.svelte
@@ -2,8 +2,8 @@
   import { createClient } from "@supabase/supabase-js";
   import { onMount } from "svelte";
   import { writable, get } from "svelte/store";
-  import { getStores, navigating, page, updated } from '$app/stores';
-  import { Pagination } from 'flowbite-svelte'
+  // import { getStores, navigating, page, updated } from '$app/stores';
+  // import { Pagination } from 'flowbite-svelte'
 
   const url = "https://ujnattukwsqsjmzuhyoh.supabase.co";
 
@@ -160,38 +160,6 @@
   onMount(() => {
     updateTotalPrice();
   });
-
-
-  $: activeUrl = $page.url.searchParams.get('page')
-  let pages = [
-    { name: 1, href: '/components/pagination?page=1'},
-    { name: 2, href: '/components/pagination?page=2'},
-    { name: 3, href: '/components/pagination?page=3'},
-    { name: 4, href: '/components/pagination?page=4'},
-    { name: 5, href: '/components/pagination?page=5'}
-  ];
-
-  $:{ 
-      pages.forEach((page)=>{
-      let splitUrl = page.href.split('?');
-      let queryString = splitUrl.slice(1).join('?');
-      const hrefParams = new URLSearchParams(queryString);
-      let hrefValue = hrefParams.get('page');
-      if ( hrefValue === activeUrl){
-        page.active=true
-      }else{
-        page.active=false
-      }
-    })
-      pages=pages
-  }
-
-  const previous = () => {
-    alert('Previous btn clicked. Make a call to your server to fetch data.');
-  };
-  const next = () => {
-    alert('Next btn clicked. Make a call to your server to fetch data.');
-  };
 </script>
 
 <main>
@@ -723,7 +691,7 @@
         </div>
       </div>
     </section>
-    <script src="pagination copy.js"></script>
+    <!-- <script src="pagination copy.js"></script> -->
     </body
   >
 </main>
